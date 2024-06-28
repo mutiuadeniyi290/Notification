95b3bb4b4c0baef31a6e37539f0b04039153549f
mutiuadeniyi290 <mutiuadeniyi290@yahoo.com>
2024-06-28 19:52:17 +0100

    new job2

--- generate.sh
+++ generate.sh
@@ -1,5 +1,16 @@
+
 #!/bin/bash
-# Generate a random version number based on the current timestamp
-VERSION=$(date +%Y%m%d%H%M%S)
+# Generate a random 4-digit version number
+VERSION=$(shuf -i 1000-9999 -n 1)
 echo "Generated version: $VERSION"
 echo "VERSION=$VERSION" >> $GITHUB_ENV
+
+
+
+
+
+# #!/bin/bash
+# # Generate a random version number based on the current timestamp
+# VERSION=$(date +%Y%m%d%H%M%S)
+# echo "Generated version: $VERSION"
+# echo "VERSION=$VERSION" >> $GITHUB_ENV
